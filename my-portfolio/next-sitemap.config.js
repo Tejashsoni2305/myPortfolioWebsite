930c68d57f076696dea990eae6c41e6161ba3f05
Tejashsoni2305 <87148307+Tejashsoni2305@users.noreply.github.com>
2025-01-16 20:26:48 -0800

    basic seo and favicon

---
+++ my-portfolio/next-sitemap.config.js
@@ -0,0 +1,7 @@
+/** @type {import('next-sitemap').IConfig} */
+module.exports = {
+    siteUrl: "https://tejashsoni.dev", // Replace with your domain
+    generateRobotsTxt: true, // Generates robots.txt file automatically
+    sitemapSize: 5000, // Split large sitemaps
+  };
+  
