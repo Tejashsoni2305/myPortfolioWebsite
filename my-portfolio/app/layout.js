f8a8e08aad97254261ece204a2ee68476836d9a9
Tejashsoni2305 <87148307+Tejashsoni2305@users.noreply.github.com>
2025-01-16 22:32:34 -0800

    speedInsights integration

--- my-portfolio/app/layout.js
+++ my-portfolio/app/layout.js
@@ -2,6 +2,7 @@ import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import { ThemeProvider } from "@/components/theme-provider"
 import Footer from "@/components/Footer";
+import { SpeedInsights } from "@vercel/speed-insights/next"
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -54,6 +55,7 @@ export default function RootLayout({ children }) {
           disableTransitionOnChange
         >
           {children}
+          <SpeedInsights />
           <Footer />
         </ThemeProvider>
       </body>
