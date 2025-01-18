c3dad3bc5630f084acb9368fdb9a146af074c86a
Tejashsoni2305 <87148307+Tejashsoni2305@users.noreply.github.com>
2025-01-18 15:59:33 -0800

    updated layout to permanent dark mode

--- my-portfolio/app/layout.js
+++ my-portfolio/app/layout.js
@@ -36,7 +36,7 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="en" suppressHydrationWarning>
+    <html lang="en" suppressHydrationWarning className="dark">
        <head>
        <link rel="icon" href="/favicon.ico" />
         <title>{metadata.title}</title>
