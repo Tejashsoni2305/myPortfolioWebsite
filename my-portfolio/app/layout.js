c1034d3e75152bd793b8f3fd4213f85c3bf10cf8
Tejashsoni2305 <87148307+Tejashsoni2305@users.noreply.github.com>
2025-01-18 16:03:00 -0800

    updated theme

--- my-portfolio/app/layout.js
+++ my-portfolio/app/layout.js
@@ -50,7 +50,7 @@ export default function RootLayout({ children }) {
       <body>
         <ThemeProvider
           attribute="class"
-          defaultTheme="system"
+          defaultTheme="dark"
           enableSystem
           disableTransitionOnChange
         >
